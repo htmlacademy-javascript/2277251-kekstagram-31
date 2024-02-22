6ce30815a0a54008890414d79b0b54c0417ae95a
Georgij Tsábiev <gtsabiev@inbox.ru>
2024-02-22 14:56:24 +0300

    Реализует функцию для генерации комментариев с условиями задания

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ const getRandomInteger = (a, b) => { // функция для генерации
   return Math.floor(result);
 };
 const commentAuthors = ['Пётр', 'Иван', 'Илья', 'Анна', 'Алиса', 'Николай'];
+
+function generateComments() { // функция для генерации комментариев
+  const numComments = getRandomInteger(0, 30);
+  const comments = [];
+  for (let i = 0; i < numComments; i++) { // цикл для генерации комментариев и записи их в массив comments.
+    const id = i;
+    const avatar = `img/avatar-${getRandomInteger(1,6)}.svg`;
+    const name = commentAuthors[getRandomInteger(0, commentAuthors.length - 1)];
+    const message = getRandomMessage();
+    comments.push({ // запись комментариев в массив comments.
+      id,
+      avatar,
+      name,
+      message
+    });
+  }
+  return comments;
+}
