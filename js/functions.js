2fe55cfff71a6cab33c82fdbeee5009b78861150
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-03-13 12:10:25 +0300

    Возвращает файл function.js для работы над ДЗ Функции возвращаются

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+// Функция для проверки длины строки.
+function getStringLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+getStringLength('проверяемая строка', 20); // true
+getStringLength('проверяемая строка', 18); // true
+getStringLength('проверяемая строка', 10); // false
+
+// Функция для проверки, является ли строка палиндромом.
+function isPalindrome(string) {
+  string = string.replaceAll(/\s/g,'').toLowerCase();
+  return string === string.split('').reverse().join('');
+}
+
+isPalindrome('топот'); // true
+isPalindrome('ДовОд'); // true
+isPalindrome('Кекс'); // false
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
+// Дополнительное задание - Функция принимающая строку и возвращающая целое число.
+function getNumber(string) {
+  const number = parseInt(string.toString().replace(/[^\d]/g, ''), 10);
+  return number;
+}
+
+getNumber('2023 год'); // 2023
+getNumber('ECMAScript 2022'); // 2022
+getNumber('1 кефир, 0.5 батона'); // 105
+getNumber('агент 007'); // 7
+getNumber('а я томат'); // NaN
