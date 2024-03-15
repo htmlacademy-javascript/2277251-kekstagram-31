76342c71342af3383cbc78685f9747885a5982ed
Keks <keks@htmlacademy.ru>
2024-03-15 15:25:57 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/thumbnails.js
@@ -0,0 +1,16 @@
+const renderThumbnails = (images) => { // Функция, отвечающая за отображение фотографий других пользователей
+  const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const thumbnailContainer = document.querySelector('.pictures');
+  const thumbnailFragment = document.createDocumentFragment();
+  images.forEach((image) => { // Перебираем изображения
+    const thumbnail = thumbnailTemplate.cloneNode(true); // Клонируем шаблон и наполняем его
+    thumbnail.querySelector('.picture__img').src = image.url;
+    thumbnail.querySelector('.picture__img').alt = image.description;
+    thumbnail.querySelector('.picture__likes').textContent = image.likes;
+    thumbnail.querySelector('.picture__comments').textContent = image.comments.length;
+    thumbnailFragment.append(thumbnail); // Добавляем миниатюру к фрагменту
+  });
+  thumbnailContainer.append(thumbnailFragment); // Добавляем фрагмент в контейнер
+};
+
+export {renderThumbnails};
