7731a4de5fb1a367d04d3348527370f7b1d9aaee
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-03-23 22:47:22 +0300

    ДÐВызываент функцию в точке вÐхода с дефольнтным раÐмастшÑштабом изображения

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createPhotos} from './data.js';
 import {renderThumbnails} from './thumbnails.js';
-import {configureFormValidation} from './form.js';
+import {configureFormValidation, updateScale} from './form.js';
 
 renderThumbnails(createPhotos());
 configureFormValidation();
+updateScale(1);
