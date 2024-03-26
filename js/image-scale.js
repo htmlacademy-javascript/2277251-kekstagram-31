d6ff18b631576cb46fcc59963db6a7eb10248bc7
Georgij Tsabiev <gtsabiev@inbox.ru>
2024-03-26 21:01:49 +0300

    Выносит в константу процентное значение отображения масштаба

--- js/image-scale.js
+++ js/image-scale.js
@@ -4,12 +4,13 @@ const scaleBigger = document.querySelector('.scale__control--bigger');
 const scaleValue = document.querySelector('.scale__control--value');
 const DEFAULT_SCALE = 1; // Значение по умолчанию для масштаба изображения
 const SCALE_STEP = 0.25; // Значение шага масштаба
+const SCALE_PERCENTAGE = 100; // Процентное значение, используемое для отображения масштаба
 const updateScale = (scale) => { // Функция для обновления отображения значения масштаба
-  scaleValue.value = `${scale * 100}%`;
+  scaleValue.value = `${scale * SCALE_PERCENTAGE}%`;
   uploadImage.style.transform = `scale(${scale})`;
 };
 const changeScale = (amount) => { // Функция для изменения значения масштаба
-  let scale = parseFloat(scaleValue.value) / 100;
+  let scale = parseFloat(scaleValue.value) / SCALE_PERCENTAGE;
   scale += amount;
   scale = Math.max(Math.min(scale, 1), SCALE_STEP); // Ограничиваем шкалу между минимально и максимально допустимыми значениями
   updateScale(scale);
