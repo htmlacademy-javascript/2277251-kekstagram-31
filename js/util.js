2606031cedb8baeb4ff42c225ce0bb198b45fc5f
Georgij Tsábiev <gtsabiev@inbox.ru>
2024-03-29 15:59:37 +0300

    Добавляет показ блока сообщения ошибки в util.js

--- js/util.js
+++ js/util.js
@@ -1,5 +1,6 @@
 import {closePopup, commentsList} from './popup.js';
 
+const ALERT_SHOW_TIME = 5000;
 const getRandomInteger = (a, b) => { // Функция для генерации случайного числа от min до max
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -19,4 +20,22 @@ const clearComments = () => {
   }
 };
 
-export {getRandomInteger, isEscapeKey, onEscapeEvent, clearComments};
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+  alertContainer.textContent = message;
+  document.body.append(alertContainer);
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {getRandomInteger, isEscapeKey, onEscapeEvent, clearComments, showAlert};
