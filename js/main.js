2461d86f454f0231a23e0252effb57ca1e77aac7
Georgij Tsábiev <gtsabiev@inbox.ru>
2024-03-29 15:00:10 +0300

    Отрисовывет изображения из сервера по ТЗ

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-import {createPhotos} from './data.js';
 import {renderThumbnails} from './thumbnails.js';
 import {configureFormValidation} from './form.js';
 
-renderThumbnails(createPhotos());
+fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json())
+  .then((images) => {
+    renderThumbnails(images);
+  });
+
+
 configureFormValidation();
