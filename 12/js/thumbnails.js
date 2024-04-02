f885e04d9316aa03f5d8150f6f6c09de348cdba9
Keks <keks@htmlacademy.ru>
2024-04-02 18:02:01 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/thumbnails.js
@@ -0,0 +1,29 @@
+import {openPopup} from './popup.js';
+
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const thumbnailContainer = document.querySelector('.pictures');
+const removeThumbnailElements = () => {
+  document.querySelectorAll('.picture').forEach((picture) => {
+    picture.remove();
+  });
+};
+const renderThumbnails = (images) => { // Функция, отвечающая за отображение фотографий других пользователей
+  removeThumbnailElements();
+  const thumbnailFragment = document.createDocumentFragment();
+  images.forEach(({url, description, likes, comments}) => { // Перебираем изображения
+    const thumbnail = thumbnailTemplate.cloneNode(true); // Клонируем шаблон и наполняем его
+    thumbnail.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      openPopup({url, description, likes, comments});
+    });
+    const thumbnailImage = thumbnail.querySelector('.picture__img');
+    thumbnailImage.src = url;
+    thumbnailImage.alt = description;
+    thumbnail.querySelector('.picture__likes').textContent = likes;
+    thumbnail.querySelector('.picture__comments').textContent = comments.length;
+    thumbnailFragment.append(thumbnail); // Добавляем миниатюру к фрагменту
+  });
+  thumbnailContainer.append(thumbnailFragment); // Добавляем фрагмент в контейнер
+};
+
+export {renderThumbnails};
