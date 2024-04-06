99d8d8adaf8a3b5d279bba4f83d4ff4655404b5c
Georgij Tsábiev <gtsabiev@inbox.ru>
2024-04-06 19:40:34 +0300

    Добавляет css свойства мастштабу изображению, а не родителю

--- js/image-scale.js
+++ js/image-scale.js
@@ -1,7 +1,7 @@
 const DEFAULT_SCALE = 1; // Значение по умолчанию для масштаба изображения
 const SCALE_STEP = 0.25; // Значение шага масштаба
 const SCALE_PERCENTAGE = 100; // Процентное значение, используемое для отображения масштаба
-const uploadImage = document.querySelector('.img-upload__preview');
+const uploadImage = document.querySelector('.img-upload__preview img');
 const scaleSmaller = document.querySelector('.scale__control--smaller');
 const scaleBigger = document.querySelector('.scale__control--bigger');
 const scaleValue = document.querySelector('.scale__control--value');
