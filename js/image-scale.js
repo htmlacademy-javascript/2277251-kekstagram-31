5298c47761838463b8d9e4385a412528893dc8bc
Georgij Tsábiev <gtsabiev@inbox.ru>
2024-04-08 15:48:54 +0300

    Удаляет повторяющееся обьявление переменной с DOM-элементом

--- js/image-scale.js
+++ js/image-scale.js
@@ -1,13 +1,14 @@
+import {uploadedImageEl} from './image-effects.js';
+
 const DEFAULT_SCALE = 1; // Значение по умолчанию для масштаба изображения
 const SCALE_STEP = 0.25; // Значение шага масштаба
 const SCALE_PERCENTAGE = 100; // Процентное значение, используемое для отображения масштаба
-const uploadImageEl = document.querySelector('.img-upload__preview img');
 const scaleSmallerEl = document.querySelector('.scale__control--smaller');
 const scaleBiggerEl = document.querySelector('.scale__control--bigger');
 const scaleValueEl = document.querySelector('.scale__control--value');
 const updateScale = (scale) => { // Функция для обновления отображения значения масштаба
   scaleValueEl.value = `${scale * SCALE_PERCENTAGE}%`;
-  uploadImageEl.style.transform = `scale(${scale})`;
+  uploadedImageEl.style.transform = `scale(${scale})`;
 };
 const changeScale = (amount) => { // Функция для изменения значения масштаба
   let scale = parseFloat(scaleValueEl.value) / SCALE_PERCENTAGE;
@@ -22,7 +23,7 @@ scaleBiggerEl.addEventListener('click', () => {
   changeScale(SCALE_STEP);
 });
 const resetScale = () => { // Функция для сброса масштаба изображения
-  uploadImageEl.style.transform = '';
+  uploadedImageEl.style.transform = '';
 };
 
 export {DEFAULT_SCALE, updateScale, resetScale};
